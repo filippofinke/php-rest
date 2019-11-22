4bf8c67479853065cdb67821579d2257d223a274
Filippo Finke <filippo.finke@samtrevano.ch>
2019-11-22 23:11:44 +0100

    Add support to response codes and style fix

--- examples/example.php
+++ examples/example.php
@@ -1,11 +1,22 @@
 <?php
 use FilippoFinke\Router;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $router = new Router();
 
-$router->get('/', function($req, $res) {
-    printf("Request from %s to %s with method %s", $req->getRemoteAddress(), $req->getUri(), $req->getMethod());
+$router->get('/', function ($req, $res) {
+    $html = sprintf("<b>Request</b> from %s to %s with method %s", $req->getRemoteAddress(), $req->getUri(), $req->getMethod());
+    return $res->withHtml($html);
+});
+
+$router->get('/json', function ($req, $res) {
+    return $res->withJson(
+        array(
+            'status' => true,
+            'json' => [1,2,3,4,5]
+        )
+    );
 });
 
 $router->start();
