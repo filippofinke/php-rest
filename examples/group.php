7ce7c27865ee5a10998b68c31843a74f9344d6fb
Filippo Finke <filippo.finke@samtrevano.ch>
2019-12-04 18:55:16 +0100

    Added group example

---
+++ examples/group.php
@@ -0,0 +1,22 @@
+<?php
+use FilippoFinke\Router;
+use FilippoFinke\Middleware\ContentLengthMiddleware;
+use FilippoFinke\RouteGroup;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$router = new Router();
+
+$group = new RouteGroup();
+$group->add($router->get('/', function($req, $res) {
+    return $res->withText('/ with GET');
+}));
+$group->add($router->post('/', function($req, $res) {
+    return $res->withText('/ with POST');
+}));
+$group->before(function ($req, $res) {
+    $res->withHeader('Test-Middlware', 'Before');
+});
+
+$router->start();
+?>
