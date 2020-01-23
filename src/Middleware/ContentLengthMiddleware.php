7d970761383eb25e52d70c3c1fa635da96b2f36c
Filippo Finke <filippo.finke@samtrevano.ch>
2020-01-23 21:14:04 +0100

    Add author header

--- src/Middleware/ContentLengthMiddleware.php
+++ src/Middleware/ContentLengthMiddleware.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * @author Filippo Finke
+ */
 namespace FilippoFinke\Middleware;
 
 class ContentLengthMiddleware
--- src/RouteGroup.php
+++ src/RouteGroup.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * @author Filippo Finke
+ */
 namespace FilippoFinke;
 
 class RouteGroup
